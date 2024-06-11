b12d4aa28a9626fc368d03efd0ef1fd610211f24
micheleFaedda <michele.faeddal@hotmail.com>
2024-06-11 18:23:28 +0200

    Update main_show_diff.cpp

--- main_show_diff.cpp
+++ main_show_diff.cpp
@@ -5,64 +5,51 @@
 
 #include <iostream>
 
-#include <cinolib/meshes/meshes.h>
-#include <cinolib/gl/glcanvas.h>
-#include <cinolib/gl/surface_mesh_controls.h>
 #include <cinolib/rationals.h>
 #include <implicit_point.h>
-#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Quotient.h>
-#include <CGAL/MP_Float.h>
-#include <CGAL/Point_3.h>
-
-#include <string>
-#include <sstream>
 using namespace cinolib;
 
 
-//create a function
-inline void createRationalPoint(){
-
-}
-
 int main(int argc, char **argv) {
-
+    //plane
     const explicitPoint3D *ir = new explicitPoint3D(0.0,0.0,0.0);
     const explicitPoint3D *is = new explicitPoint3D(4.0,4.0,0.0);
     const explicitPoint3D *it = new explicitPoint3D(0.0,4.0,0.0);
 
-
+    //line to generate P0
     const explicitPoint3D *iq_P0 = new explicitPoint3D(0.5,0.5,-1.0);
     const explicitPoint3D *ip_P0 = new explicitPoint3D(0.5,0.5,1.0);
 
+    //line to generate P1
     const explicitPoint3D *iq_P1 = new explicitPoint3D(0.5,1,-1.0);
     const explicitPoint3D *ip_P1 = new explicitPoint3D(0.5,1,1.0);
 
+    //line to generate P2
     const explicitPoint3D *iq_P2 = new explicitPoint3D(1.0,1.0,-1.0);
     const explicitPoint3D *ip_P2 = new explicitPoint3D(1.0,1.0,1.0);
 
+    //line to generate P3
     const explicitPoint3D *iq_P3 = new explicitPoint3D(1.0,0.5,-1.0);
     const explicitPoint3D *ip_P3 = new explicitPoint3D(1.0,0.5,1.0);
 
+    //create 4 implicit points
     genericPoint *P0 = new implicitPoint3D_LPI(*iq_P0, *ip_P0, *ir, *is, *it);
     genericPoint *P1 = new implicitPoint3D_LPI(*iq_P1, *ip_P1, *ir, *is, *it);
     genericPoint *P2 = new implicitPoint3D_LPI(*iq_P2, *ip_P2, *ir, *is, *it);
     genericPoint *P3 = new implicitPoint3D_LPI(*iq_P3, *ip_P3, *ir, *is, *it);
 
     /************************* ORIENT3D IMPLICIT **********************/
-    int T0_orient= genericPoint::orient3D(*P0, *ir, *it, *is);
-    int T1_orient = genericPoint::orient3D(*P1, *ir, *it, *is );
-    int T2_orient = genericPoint::orient3D(*P2, *ir, *it, *is );
-    int T3_orient = genericPoint::orient3D(*P3, *ir, *it, *is );
-
+    int T0_orient = genericPoint::orient3D(*P0, *ir, *it, *is);
+    int T1_orient = genericPoint::orient3D(*P1, *ir, *it, *is);
+    int T2_orient = genericPoint::orient3D(*P2, *ir, *it, *is);
+    int T3_orient = genericPoint::orient3D(*P3, *ir, *it, *is);
 
     std::cout << "Result P0 Implicit: " << T0_orient << std::endl;
     std::cout << "Result P1 Implicit: " << T1_orient << std::endl;
     std::cout << "Result P2 Implicit: " << T2_orient << std::endl;
     std::cout << "Result P3 Implicit: " << T3_orient << std::endl;
 
-    /*RAZIONALI*/
+    /*Rationals*/
     bigrational x_P0, y_P0, z_P0;
     P0->getExactXYZCoordinates(x_P0,y_P0,z_P0);
 
@@ -84,7 +71,7 @@ int main(int argc, char **argv) {
     bigrational x_it, y_it, z_it;
     it->getExactXYZCoordinates(x_it,y_it,z_it);
 
-    /************************* ORIENT3D RATIONALS **********************/
+    //creating the rational points
     std::vector<bigrational> P0_rational = {x_P0, y_P0, z_P0};
     std::vector<bigrational> P1_rational = {x_P1, y_P1, z_P1};
     std::vector<bigrational> P2_rational = {x_P2, y_P2, z_P2};
@@ -94,6 +81,7 @@ int main(int argc, char **argv) {
     std::vector<bigrational> is_rational = {x_is, y_is, z_is};
     std::vector<bigrational> it_rational = {x_it, y_it, z_it};
 
+    /************************* ORIENT3D RATIONALS **********************/
     auto result_rational_P0 = orient3d(&P0_rational[0], &ir_rational[0], &it_rational[0], &is_rational[0]);
     auto result_rational_P1 = orient3d(&P1_rational[0], &ir_rational[0], &it_rational[0], &is_rational[0]);
     auto result_rational_P2 = orient3d(&P2_rational[0], &ir_rational[0], &it_rational[0], &is_rational[0]);
@@ -103,7 +91,6 @@ int main(int argc, char **argv) {
     std::cout << "Result P1 Rationals: " << result_rational_P1 << std::endl;
     std::cout << "Result P2 Rationals: " << result_rational_P2 << std::endl;
     std::cout << "Result P3 Rationals: " << result_rational_P3 << std::endl;
-
 
     return 0;
 
